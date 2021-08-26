f21eb75fe7fc38ee52292c0d4df6dca17c71208a
José Hernández <jonandez91@gmail.com>
2021-08-26 12:58:29 -0300

    some fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
+# Create prometheus app
+echo "[TASK 1 ] Create Prometheus app"
+# kubectl create -f namespace.yaml
+# kubectl create -f clusterRole.yaml
+# kubectl create -f config-map.yaml
+# kubectl create -f prometheus-deployment.yaml
+# kubectl create -f prometheus-service.yaml --namespace=monitoring
+kubectl apply -f prometheus/
 
-kubectl apply -f /prometheus
-kubectl apply -f /grafana
-kubectl apply -f /state
+
+# Create prometheus app
+echo "[TASK 2 ] Kube State Metrics"
+kubectl apply -f state/
+
+
+echo "[TASK 2 ] Create Grafana app"
+kubectl apply -f grafana/
